6c087e894b17d143caef2ade26e07cd243fddbad
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-02-04 21:38:24 +0530

    Ex 1.1-1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,46 @@
+const Header = (props) => {
+  return (<h1>{props.course}</h1>)
+};
+
+const Part = (props) => {
+  return (<p>{props.part} {props.excercise}</p>)
+};
+
+const Content = (props) => {
+  return(
+    <div>
+    <Part part={props.parts[0].part} excercise={props.parts[0].Excercise} />
+    <Part part={props.parts[1].part} excercise={props.parts[1].Excercise} />
+    <Part part={props.parts[2].part} excercise={props.parts[2].Excercise} />
+    </div>
+  )
+};
+
+const Total = (props) => {
+  return(
+    <p>Total is: {props.parts[0].Excercise + props.parts[1].Excercise + props.parts[2].Excercise}
+    </p>
+  )
+};
+
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {part: "Fundamentals of React", Excercise: 10},
+      {part: "Using props to pass data", Excercise: 7},
+      {part: "State of a component", Excercise: 14}
+    ]
+
+  };
 
   return (
     <div>
-      <p>{friends[0].name} and {friends[0].age}</p>
-      <p>{friends[1].name} and {friends[1].age}</p>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
-}
+};
 
 export default App
