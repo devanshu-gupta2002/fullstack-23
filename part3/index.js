65e097ad78139238500dfb36ae73fd4b075e56b3
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-03-31 14:21:38 +0530

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -5,15 +5,6 @@ var morgan = require('morgan')
 const cors=require('cors')
 const Person = require('./module/person')
 
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-  if(error.name === 'CastError')
-  {
-    return response.status(400).send({error: 'malformatted id'})
-  }
-  next(error)
-}
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
 }
@@ -62,7 +53,7 @@ app.delete('/api/persons/:id', (request,response,next) => {
   .catch(error => next(error))  
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body=request.body
 
   if(body.name===undefined)
@@ -87,16 +78,14 @@ app.post('/api/persons', (request, response) => {
   })
   person.save().then(savedPerson => {
   response.json(savedPerson)
-  })  
+  })
+  .catch(error => next(error))  
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  const person = { 
-    name: body.name,
-    number: body.number,
-  }
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  const {name, number} = request.body
+  
+  Person.findByIdAndUpdate(request.params.id, {name, number}, {new: true, runValidators: true, context: "query"})
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -104,6 +93,17 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if(error.name === 'CastError')
+  {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
+  next(error)
+}
 app.use(errorHandler)
 
 const PORT = process.env.PORT
--- part3/module/person.js
+++ part3/module/person.js
@@ -12,9 +12,28 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB', error.message)
     })
 
+// const NumberValidator = [
+//     {
+//         validator: (number) => {
+//             if((number[2]==='-' || number[3]==='3') && number.length<9){
+//                 return false
+//             }
+//             return true
+//         }, msg: 'Number should be atleast 8 digits'
+//     }
+// ]
+
 const perSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    },
+    number: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
 })
 
 perSchema.set('toJSON', {
