f9747a45a339b8ab879eec08b13a2190193dabf9
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-09-30 00:25:29 +0530

    completed ex 13.4

--- part13/blog/index.js
+++ part13/blog/index.js
@@ -37,9 +37,30 @@ Blog.init({
   modelName: 'Blog'
 });
 
+Blog.sync()
+
 app.get('/api/blogs', async(req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
+})
+
+app.post('/api/blogs', async(req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async(req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if(blog) {
+    await blog.destroy()
+    return res.status(204).end()
+  } else {
+    return res.status(404).end()
+  }
 })
 
 const PORT = process.env.PORT || 3001
